e0c66cc50e1e377e467aa4c7576df30f1080ed2e
Jeff Lopes <jlopes@monkeycity.org>
2013-07-18 11:43:37 -0400

    use daemon_id in manual feedback call

--- lib/rapns/apns_feedback.rb
+++ lib/rapns/apns_feedback.rb
@@ -3,7 +3,7 @@ module Rapns
     Rapns.require_for_daemon
     Rapns::Daemon.initialize_store
 
-    Rapns::Apns::App.all.each do |app|
+    Rapns::Apns::App.for_daemon_id(Rapns.config.daemon_id).each do |app|
       receiver = Rapns::Daemon::Apns::FeedbackReceiver.new(app, 0)
       receiver.check_for_feedback
     end
