ae7b50765a83520b5b0e86b861e48c054c0867b5
Jeff Lopes <jlopes@monkeycity.org>
2013-08-11 20:14:22 -0400

    unique indexes for notifications table

--- lib/generators/templates/add_job_to_rapns.rb
+++ lib/generators/templates/add_job_to_rapns.rb
@@ -2,10 +2,14 @@ class AddJobToRapns < ActiveRecord::Migration
   def self.up
     add_column :rapns_notifications, :job_id, :integer, :null => true
     add_column :rapns_apps, :job_id, :integer, :null => true
+    add_index :rapns_notifications, [:job_id, :device_token], :unique => true, :name => "index_rapns_notifications_unique_apns"
+  	add_index :rapns_notifications, [:job_id, :registration_ids], :unique => true, :name => "index_rapns_notifications_unique_gcm", :length => {:registration_ids => 250}
   end
 
   def self.down
-    remove_column :rapns_notifications, :job_id
+  	remove_index :rapns_notifications, :name => "index_rapns_notifications_unique_gcm"
+    remove_index :rapns_notifications, :name => "index_rapns_notifications_unique_apns"
     remove_column :rapns_apps, :job_id
+    remove_column :rapns_notifications, :job_id
   end
 end
