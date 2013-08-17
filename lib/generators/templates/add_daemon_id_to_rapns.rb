6170bf5c2f3e1fe64bd583ca0a934b5917499d4b
Jeff Lopes <jlopes@monkeycity.org>
2013-08-16 23:48:16 -0400

    add index for daemon_id

--- lib/generators/templates/add_daemon_id_to_rapns.rb
+++ lib/generators/templates/add_daemon_id_to_rapns.rb
@@ -2,9 +2,11 @@ class AddDaemonIdToRapns < ActiveRecord::Migration
   def self.up
     add_column :rapns_notifications, :daemon_id, :string, :null => true
     add_column :rapns_apps, :daemon_id, :string, :null => true
+    add_index :rapns_notifications, :daemon_id, :name => "index_rapns_notifications_daemon_id"
   end
 
   def self.down
+  	remove_index :rapns_notifications, :name => "index_rapns_notifications_daemon_id"
     remove_column :rapns_notifications, :daemon_id
 	remove_column :rapns_apps, :daemon_id    
   end
